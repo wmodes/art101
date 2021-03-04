60d3f95223943cb014c2f9ca358b6919cbe54dab
Wes Modes <wmodes@gmail.com>
2021-03-04 11:55:36 -0800

    lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -5,7 +5,7 @@
  */
 
 //apiEndpoint = "https://api.chucknorris.io/jokes/random";
-apiEndpoint = "https://xkcd.com/info.0.json"
+var apiEndpoint = "https://xkcd.com/info.0.json"
 
 $("#api-button").click(doAPIStuff);
 
@@ -21,20 +21,28 @@ function doAPIStuff() {
     // The data to send (will be converted to a query string)
     data: { },
     // Whether this is a POST or GET request
-    type: "GET"
+    type: "GET",
     // The type of data we expect back
-    //dataType : "json",
-  })
-  // If the request succeeds
-  .done(function( data ) {
-    var textData = JSON.stringify(data);
-    var str = "";
-    var imageURL = data.img;
-    var title = data.title;
-    str += "<h2>" + title + "</h1>";
-    str += "<img src='" + imageURL + "'>"
-    console.log("Success:", textData);
-    putTextOnPage(str);
+    dataType : "json",
+    // If the request succeeds
+    success: function( data ) {
+      // turn data object into string just so we can display it
+      // and look at it and understand WTF is going on.
+      var textData = JSON.stringify(data);
+      // let's build up a string to display on our page
+      var str = "";
+      // let's get the individual values from data object
+      var imageURL = data.img;
+      var title = data.title;
+      var alt = data.alt;
+      // building our string for display using HTML
+      str += "<h2>" + title + "</h1>";
+      str += "<img src='" + imageURL + "' title='" + alt + "'>"
+      // put our html string on our page using our helper function
+      putTextOnPage(str);
+      // on the console, we can see whaz up
+      console.log("Success:", textData);
+    }
   })
   // If the request fails
   .fail(function( xhr, status, errorThrown ) {
