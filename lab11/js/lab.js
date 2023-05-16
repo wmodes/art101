05015df55166477d845ab1d103f38406e999b521
Wes Modes <wmodes@gmail.com>
2023-05-16 13:08:28 -0700

    small additons

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -8,9 +8,7 @@
 // Create the button
 // Give it a text label
 // Add it to the section
-$("#challenge").append("<button id='c-button'>Button</button>");
-$("#problem").append("<button id='p-button'>Button</button>");
-$("#results").append("<button id='r-button'>Button</button>");
+$(".sectionbox").append("<button class='btn btn-secondary highlight-button'>Toggle Highlight</button>");
 
 // Add to your previous JavaScript file:
 // Using jQuery, do the following for each button:
@@ -19,23 +17,23 @@ $("#results").append("<button id='r-button'>Button</button>");
 // Toggle a class special within the <div> of the section
 // Go into your css and style each of the classes
 // Test each button
-$("#c-button").click(function(){
-  // find the section, toggle a class
-  // $("#challenge").toggleClass("special");
-
+$(".highlight-button").click(function(){
+  // I can use "this" to refer to the button that got clicked
   // here's a different maybe better way to do it
   // (we are traversing the DOM to the parent)
   $(this).parent().toggleClass("special");
 })
 
-$("#p-button").click(function(){
-  // here's a different maybe better way to do it
-  // (we are traversing the DOM to the parent)
-  $(this).parent().toggleClass("special");
-})
-
-$("#r-button").click(function(){
-  // here's a different maybe better way to do it
-  // (we are traversing the DOM to the parent)
-  $(this).parent().toggleClass("special");
+// Now let's add a handler for the party toggle button
+$("#party-toggle button").click(function(){
+  // if the body element has a class of party...
+  // then change button text to "Business Time"
+  if ($("body").hasClass("party")) {
+    $("body").removeClass("party");
+    $("#party-toggle button").html("Party");
+  }
+  else {
+    $("body").addClass("party");
+    $("#party-toggle button").html("Business");
+  }
 })
