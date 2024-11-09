aff50f19ed94e5c11e1989b5a68e6d96b0ec26fb
Wes Modes <wmodes@gmail.com>
2024-11-08 20:36:32 -0800

    Update lab.js

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -8,9 +8,9 @@
 */
 
 function generateRandomText() {
-  const text = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.";
+  const text = `It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only. There were a king with a large jaw and a queen with a plain face, on the throne of England; there were a king with a large jaw and a queen with a fair face, on the throne of France. In both countries it was clearer than crystal to the lords of the State preserves of loaves and fishes, that things in general were settled for ever.`;
   const min = 3;
-  const max = 100;
+  const max = 150;
   const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
   // Get a random starting index and adjust to nearest whitespace boundary
   let randStart = Math.floor(Math.random() * (text.length - randLen + 1));
@@ -23,7 +23,9 @@ function generateRandomText() {
     endIdx++;
   }
   // Generate the random text, breaking at the nearest whitespace boundaries
-  return text.slice(randStart, endIdx).trim();
+  let selectedText = text.slice(randStart, endIdx).trim();
+  // Capitalize the first letter of the selected text
+  return selectedText.charAt(0).toUpperCase() + selectedText.slice(1);
 }
 
 
